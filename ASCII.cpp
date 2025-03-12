008df203253e3fba618b44ce75093b050f777759
kimtaejung <taejung4098@naver.com>
2025-03-12 21:05:02 +0900

    first commit

---
+++ ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "  III   L      OOO  V   V  EEEEE   K   K  TTTTT  JJJJJ  " << endl;
+    cout << "   I    L     O   O V   V  E       K  K     T      J   " << endl;
+    cout << "   I    L     O   O V   V  EEEE    KK       T      J   " << endl;
+    cout << "   I    L     O   O  V V   E       K  K     T   J  J   " << endl;
+    cout << "  III   LLLLL  OOO    V    EEEEE   K   K    T    JJ    " << endl;
+
+    return 0;
+}
